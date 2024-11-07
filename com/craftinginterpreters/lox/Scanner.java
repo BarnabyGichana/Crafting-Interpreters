0d16ecb0731bd5b8f112d51505e0a9e3eac0fe86
Barnaby <gichana@andrews.edu>
2024-11-06 20:56:56 -0500

    Added reserved words and identifiers as keywords to the scanner.

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -22,6 +22,27 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",     AND);
+        keywords.put("class",   CLASS);
+        keywords.put("else",    ELSE);
+        keywords.put("false",   FALSE);
+        keywords.put("for",     FOR);
+        keywords.put("fun",     FUN);
+        keywords.put("if",      IF);
+        keywords.put("nil",     NIL);
+        keywords.put("or",      OR);
+        keywords.put("print",   PRINT);
+        keywords.put("return",  RETURN);
+        keywords.put("super",   SUPER);
+        keywords.put("this",    THIS);
+        keywords.put("true",    TRUE);
+        keywords.put("var",     VAR);
+        keywords.put("while",   WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -88,12 +109,23 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
                 break;
             //< Handling errors at the lexical level.
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
     }
 
     private void number() {
@@ -139,7 +171,6 @@ class Scanner {
         return true;
     }
 
-    // Lookahead.
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
@@ -150,6 +181,16 @@ class Scanner {
         return source.charAt(current + 1);
     }
 
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
     private boolean isDigit(char c) {
         return c >= '0' && c <= '9';
     }
@@ -158,13 +199,10 @@ class Scanner {
         return current >= source.length();
     }
 
-    //> Consumes the next character in the source file and returns it.
     private char advance() {
         return source.charAt(current++);
     }
-    //< Input.
 
-    //> Output.
     private void addToken(TokenType type) {
         addToken(type, null);
     }
@@ -173,5 +211,4 @@ class Scanner {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
-    //< Output.
 }
