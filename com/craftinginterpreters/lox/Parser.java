4738a49a8ef7d06f6e776be2beae33377bf96ddc
Barnaby <gichana@andrews.edu>
2025-03-02 00:30:20 -0500

    Syntax errors, your days are numbered! Initialized the implimentation of panic mode error recovery and descent parser synchronization. There is still work to be done in regards to this since the language does not have statements at this point. Once statements are added to the language, then the relevant methods and classes will be reworked as needed.

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -5,11 +5,24 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+    private static class ParseError extends RuntimeException {}
+
     private final List<Token> tokens;
     private int current = 0;
 
     Parser(List<Token> tokens) {
         this.tokens = tokens;
+    }
+
+    // This "parse" method will be revisited later when
+    // statements are added to the language. For now it parses a
+    // single expression and returns it.
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
     }
 
     private Expr expression() {
@@ -67,7 +80,7 @@ class Parser {
     private Expr unary() {
         if (match(BANG, MINUS)) {
             Token operator = previous();
-            Expr right = unary;
+            Expr right = unary();
             return new Expr.Unary(operator, right);
         }
 
@@ -86,8 +99,10 @@ class Parser {
         if (match(LEFT_PAREN)) {
             Expr expr = expression();
             consume(RIGHT_PAREN, "Expect ')' after expression.");
-            reutrn new Expr.Grouping(expr);
+            return new Expr.Grouping(expr);
         }
+
+        throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types) {
@@ -99,6 +114,12 @@ class Parser {
         }
 
         return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+
+        throw error(peek(), message);
     }
 
     private boolean check(TokenType type) {
@@ -121,5 +142,35 @@ class Parser {
 
     private Token previous() {
         return tokens.get(current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+
+            // The following "switch" statement is not yet complete
+            // and will likely be completed in a later chapter.
+            // Current chapter: 6
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
+
+            advance();
+        }
     }
 }
